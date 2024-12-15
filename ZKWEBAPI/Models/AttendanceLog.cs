9840a9dffd7347b8f90a2c7442f8706391840a7f
Maksat Oğuzhan Garyagdyyev <garyagdyyev@outlook.com>
2024-12-15 14:54:13 +0300

    AttendanceLog model added

---
+++ ZKWEBAPI/Models/AttendanceLog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZKWEBAPI.Models
+{
+    public class AttendanceLog
+    {
+        public string User { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
